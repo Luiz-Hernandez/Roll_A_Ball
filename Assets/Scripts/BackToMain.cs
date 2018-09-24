d5a873d5b4cd5ec1a8e1afa2a2253b8cfa9e4df8
Luiz-Hernandez <43526109+Luiz-Hernandez@users.noreply.github.com>
2018-09-23 19:35:55 -0700

    log

---
+++ Assets/Scripts/BackToMain.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToMain : MonoBehaviour {
+
+    public void BackMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+}
